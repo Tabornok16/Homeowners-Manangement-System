cbf5681f4a4ce42d48a8b3a408a52cbc4757abb3
Jaycey Velandres <jvelandres@gmail.com>
2024-04-04 16:24:00 +0800

    Event in Calendar

--- homeowner/partial/navbar.php
+++ homeowner/partial/navbar.php
@@ -5,25 +5,72 @@
     <li class="nav-item">
       <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
     </li>
+    <li class="nav-item dropdown">
+      <a id="dropdownSubMenu1" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link dropdown-toggle">Transaction</a>
+      <ul aria-labelledby="dropdownSubMenu1" class="dropdown-menu border-0 shadow">
+
+        <li><a href="#" class="dropdown-item">Homeowner Verification</a></li>
+        <li><a href="#" class="dropdown-item">Posting Announcement</a></li>
+
+
+
+        <!-- Level two dropdown-->
+        <li class="dropdown-submenu dropdown-hover">
+          <a id="dropdownSubMenu2" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="dropdown-item dropdown-toggle">Payment</a>
+          <ul aria-labelledby="dropdownSubMenu2" class="dropdown-menu border-0 shadow">
+            <li>
+              <a tabindex="-1" href="#" class="dropdown-item">HOA Dues</a>
+            </li>
+
+            <!-- Level three dropdown -->
+            <li class="dropdown-submenu">
+              <a id="dropdownSubMenu3" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="dropdown-item dropdown-toggle">Miscellaneous</a>
+              <ul aria-labelledby="dropdownSubMenu3" class="dropdown-menu border-0 shadow">
+                <li><a href="#" class="dropdown-item">RFID</a></li>
+                <li><a href="#" class="dropdown-item">Car Sticker</a></li>
+                <li><a href="#" class="dropdown-item">Reservation</a></li>
+                <li><a href="#" class="dropdown-item">Penalty</a></li>
+                <li class="dropdown-submenu"> <!-- New dropdown submenu for "Construction" -->
+                  <a id="dropdownSubMenu4" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="dropdown-item dropdown-toggle">Construction</a>
+                  <ul aria-labelledby="dropdownSubMenu4" class="dropdown-menu border-0 shadow">
+                    <li><a href="#" class="dropdown-item">Processing Fee</a></li>
+                    <li><a href="#" class="dropdown-item">Bond</a></li>
+                    <li><a href="#" class="dropdown-item">Delivery Fee</a></li>
+                    <li><a href="#" class="dropdown-item">Worker's ID</a></li>
+                    <!-- Add more construction-related items as needed -->
+                  </ul>
+                </li>
+                <li><a href="#" class="dropdown-item">Other</a></li>
+              </ul>
+            </li>
+            <!-- End Level three -->
+
+          </ul>
+        </li>
+        <!-- End Level two -->
+        <li><a href="#" class="dropdown-item">Transaction History</a></li>
+      </ul>
+    </li>
   </ul>
+
 
   <!-- Right navbar links -->
   <ul class="navbar-nav ml-auto">
     <li class="nav-item mr-lg-3">
-        <a class="nav-link" href="calendar.php" style="font-size: 18px;">
+    <a class="nav-link" href="calendar.php" style="font-size: 18px;">
             <i class="ion-calendar"></i>
         <?php
           date_default_timezone_set('Asia/Manila');
           $currentDateTime = date('l, F d, Y H:i');
           echo $currentDateTime;
-
         ?>
       </a>
     </li>
     <li class="nav-item">
-        <a class="nav-link" href="../logout.php" style="font-size: 18px;">
-      <i class="fas fa-power-off" style="color: orange;"></i> Log Out
-    </a>
+    <a class="nav-link" href="../logout.php" style="font-size: 18px;">
+  <i class="fas fa-power-off" style="color: orange;"></i> Log Out
+</a>
+      </a>
     </li>
-  </ul>     
+  </ul>
 </nav>
