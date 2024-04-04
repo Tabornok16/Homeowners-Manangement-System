f04c487e09f856695800445f523e2deefc485f1c
Jaycey Velandres <jvelandres@gmail.com>
2024-04-04 23:13:15 +0800

    dashboard data

--- admin/index.php
+++ admin/index.php
@@ -25,63 +25,162 @@
   <!-- Main content -->
   <section class="content">
     <div class="container-fluid">
-      <!-- Small boxes (Stat box) -->
+
+      <!-- Total Number of Homeowners -->
       <div class="row">
         <div class="col-lg-3 col-6">
           <!-- small box -->
           <div class="small-box bg-info">
-            <div class="inner">
-              <h3>150</h3>
-              <p>TOTAL HOMEOWNERS</p>
-            </div>
+          <div class="inner">
+          <?php
+            // Include connect.php to establish database connection
+            $servername = "localhost";
+            $username = "root";
+            $password = "";
+            $dbname = "hoa_db";
+
+            // Create connection
+            $conn = new mysqli($servername, $username, $password, $dbname);
+
+            // Check connection
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            }
+
+            // Check if $conn is defined and not null
+            if ($conn) {
+                // Query to count total users
+                $sql = "SELECT COUNT(*) AS totalUsers FROM user";
+
+                // Execute the query
+                $result = $conn->query($sql);
+
+                if ($result) {
+                    // Fetch the result as an associative array
+                    $row = $result->fetch_assoc();
+
+                    // Check if the result is not empty
+                    if ($row) {
+                        // Get the total users count
+                        $totalUsers = $row['totalUsers'];
+
+                        // Display the total users count
+                        echo '<h3>' . $totalUsers . '</h3>';
+                    } else {
+                        // If the result is empty, display an error message or default value
+                        echo '<h3>Error</h3>';
+                    }
+                } else {
+                    // If query execution fails, display an error message or default value
+                    echo '<h3>Error</h3>';
+                }
+
+                // Close the database connection
+                $conn->close();
+            } else {
+                // If $conn is not defined or null, display an error message
+                echo '<h3>Error: Database connection failed.</h3>';
+            }
+            ?>
+      <p>Total Number Homeowners Registered</p>
+    </div>
             <div class="icon">
-              <i class="ion ion-bag"></i>
+            <i class="ion ion-person"></i>
             </div>
-            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            <a href="user.view.php" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
           </div>
         </div>
+<!--End of Total number of Homeowners-->
+
+<!--Total Number of Registered Properties-->
+<div class="col-lg-3 col-6">
+  <!-- small box -->
+  <div class="small-box bg-success">
+    <div class="inner">
+            <?php
+            // Include connect.php to establish database connection
+            $servername = "localhost";
+            $username = "root";
+            $password = "";
+            $dbname = "hoa_db";
+
+            // Create connection
+            $conn = new mysqli($servername, $username, $password, $dbname);
+
+            // Query to count total registered properties
+            $sql = "SELECT COUNT(*) AS totalProperties FROM property";
+
+            // Execute the query
+            $result = $conn->query($sql);
+
+            if ($result) {
+              // Fetch the result as an associative array
+              $row = $result->fetch_assoc();
+
+              // Check if the result is not empty
+              if ($row) {
+                // Get the total properties count
+                $totalProperties = $row['totalProperties'];
+
+                // Display the total properties count
+                echo '<h3>' . $totalProperties . '</h3>';
+              } else {
+                // If the result is empty, display an error message or default value
+                echo '<h3>Error</h3>';
+              }
+            } else {
+              // If query execution fails, display an error message or default value
+              echo '<h3>Error</h3>';
+            }
+
+            // Close database connection
+            $conn->close();
+            ?>
+            <p>Total Number Registered Properties</p>
+          </div>
+          <div class="icon">
+            <i class="fas fa-home"></i>
+          </div>
+          <a href="property.view.php" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+        </div>
+      </div>
+<!--End Total Number of Registered Properties-->
+
+
+
+
+        
         <!-- ./col -->
         <div class="col-lg-3 col-6">
           <!-- small box -->
-          <div class="small-box bg-success">
+          <div class="small-box bg-orange">
             <div class="inner">
-              <h3>53<sup style="font-size: 20px">%</sup></h3>
+              <h3>44</h3>
 
-              <p>Total Number of Verified</p>
+              <p>Total Transactions</p>
             </div>
             <div class="icon">
-              <i class="ion ion-stats-bars"></i>
+            <i class="fas fa-exchange-alt"></i>
             </div>
             <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
           </div>
         </div>
+
+
+
+        
         <!-- ./col -->
         <div class="col-lg-3 col-6">
+          <!-- small box -->
           <!-- small box -->
           <div class="small-box bg-warning">
             <div class="inner">
               <h3>44</h3>
 
-              <p>User Registrations</p>
+              <p>Total Number of Reservations</p>
             </div>
             <div class="icon">
-              <i class="ion ion-person-add"></i>
-            </div>
-            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-          </div>
-        </div>
-        <!-- ./col -->
-        <div class="col-lg-3 col-6">
-          <!-- small box -->
-          <!-- small box -->
-          <div class="small-box bg-warning">
-            <div class="inner">
-              <h3>44</h3>
-
-              <p>Total Number of Transactions</p>
-            </div>
-            <div class="icon">
-              <i class="ion ion-person-add"></i>
+            <i class="fas fa-calendar-check"></i>
             </div>
             <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
           </div>
