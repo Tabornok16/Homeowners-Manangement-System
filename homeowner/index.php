5e337a05b27a560fcf5a766d4a4e7eb4c3ac30c2
Jaycey Velandres <jvelandres@gmail.com>
2024-04-05 16:21:40 +0800

    icon homeowner user

--- homeowner/index.php
+++ homeowner/index.php
@@ -33,10 +33,10 @@
             <div class="inner">
               <h3>150</h3>
 
-              <p>New Orders</p>
+              <p>Total Amount Transactions</p>
             </div>
             <div class="icon">
-              <i class="ion ion-bag"></i>
+              <i class="fas fa-exchange-alt"></i>
             </div>
             <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
           </div>
@@ -48,10 +48,10 @@
             <div class="inner">
               <h3>53<sup style="font-size: 20px">%</sup></h3>
 
-              <p>Bounce Rate</p>
+              <p>Payment Deliquency</p>
             </div>
             <div class="icon">
-              <i class="ion ion-stats-bars"></i>
+            <i class="fas fa-exclamation-triangle"></i>
             </div>
             <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
           </div>
@@ -63,10 +63,10 @@
             <div class="inner">
               <h3>44</h3>
 
-              <p>User Registrations</p>
+              <p>Collections Report</p>
             </div>
             <div class="icon">
-              <i class="ion ion-person-add"></i>
+            <i class="fas fa-file-invoice-dollar"></i>
             </div>
             <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
           </div>
--- staff/index.php
+++ staff/index.php
@@ -14,7 +14,7 @@
         <div class="col-sm-6">
           <ol class="breadcrumb float-sm-right">
             <li class="breadcrumb-item"><a href="#">Home</a></li>
-            <li class="breadcrumb-item active">Dashboard</li>
+            <li class="breadcrumb-item active">Staff Dashboard</li>
           </ol>
         </div><!-- /.col -->
       </div><!-- /.row -->
