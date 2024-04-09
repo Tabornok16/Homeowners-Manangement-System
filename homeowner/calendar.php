987006fbdc635140065933312df9a920535bf3d2
Jaycey Velandres <jvelandres@gmail.com>
2024-04-09 19:12:39 +0800

    Update calendar.php

--- homeowner/calendar.php
+++ homeowner/calendar.php
@@ -1,6 +1,6 @@
-<?include("partial/header.php");?>
-<?include("partial/navbar.php");?>
-<?include("partial/sidebar.php");?>
+<?php include("partial/header.php"); ?>
+<?php include("partial/navbar.php"); ?>
+<?php include("partial/sidebar.php"); ?>
 
 
 <!DOCTYPE html>
@@ -31,35 +31,7 @@
 </head>
 <body class="hold-transition sidebar-mini">
 <div class="wrapper">
-  <!-- Navbar -->
-<!-- Navbar -->
-<nav class="main-header navbar navbar-expand-lg navbar-white navbar-light">
-  <!-- Left navbar links -->
-  <ul class="navbar-nav">
-    <li class="nav-item">
-      <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-    </li>
-  </ul>
 
-  <!-- Right navbar links -->
-  <ul class="navbar-nav ml-auto">
-    <li class="nav-item mr-lg-3">
-        <a class="nav-link" href="calendar.php" style="font-size: 18px;">
-            <i class="ion-calendar"></i>
-        <?php
-          date_default_timezone_set('Asia/Manila');
-          $currentDateTime = date('l, F d, Y H:i');
-          echo $currentDateTime;
-        ?>
-      </a>
-    </li>
-    <li class="nav-item">
-    <a class="nav-link" href="../logout.php" style="font-size: 18px;">
-  <i class="fas fa-power-off" style="color: orange;"></i> Log Out
-</a>
-    </li>
-  </ul>     
-</nav>
 
   <!-- /.navbar -->
 
