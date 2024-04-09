8d5feb57c5a7809f9aa3ff6b04aaf80636c7b8e0
Jaycey Velandres <jvelandres@gmail.com>
2024-04-09 14:47:49 +0800

    verify homeowner - staff

--- staff/partial/navbar.php
+++ staff/partial/navbar.php
@@ -9,7 +9,7 @@
       <a id="dropdownSubMenu1" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link dropdown-toggle"><strong>Transaction</strong></a>
       <ul aria-labelledby="dropdownSubMenu1" class="dropdown-menu border-0 shadow">
 
-        <li><a href="user.view.php" class="dropdown-item">Homeowner Verification</a></li>
+        <li><a href="verify_homeowner.php" class="dropdown-item">Homeowner Verification</a></li>
         <li><a href="#" class="dropdown-item">Posting Announcement</a></li>
         <!-- <li><a href="./invoice.php" class="dropdown-item">Invoice</a></li> -->
 
