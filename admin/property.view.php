c918d11f3ebca2da5abeb45c43eeca5b458ccde2
Jaycey Velandres <jvelandres@gmail.com>
2024-04-11 00:41:56 +0800

    Update property.view.php

--- admin/property.view.php
+++ admin/property.view.php
@@ -27,7 +27,7 @@
         <!-- Default box -->
         <div class="card">
             <div class="card-header">
-                <h3 class="card-title">Title</h3>
+                <h3 class="card-title">Homeowners' Property List</h3>
 
                 <div class="card-tools">
                     <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
@@ -65,7 +65,7 @@
             </div>
             <!-- /.card-body -->
             <div class="card-footer">
-                Footer
+                
             </div>
             <!-- /.card-footer-->
         </div>
