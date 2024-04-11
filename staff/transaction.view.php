f09dd69eec49244fe50afa6dc53dc63274f41230
Jaycey Velandres <jvelandres@gmail.com>
2024-04-11 16:56:50 +0800

    Update transaction.view.php

--- staff/transaction.view.php
+++ staff/transaction.view.php
@@ -1,6 +1,7 @@
 <?php include('partial/header.php'); ?>
 <?php include('partial/navbar.php'); ?>
 <?php include('partial/sidebar.php'); ?>
+?>
 
 <!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
@@ -23,13 +24,12 @@
 
     <!-- Main content -->
     <section class="content">
-
-        <!-- Default box -->
-        <div class="card">
-            <div class="card-body">
-                <!-- DataTable for Users -->
-               <div class="table-responsive">
-               <table id="stakeholderTable" class="table table-bordered table-striped table-hover">
+    <!-- Default box -->
+    <div class="card">
+        <div class="card-body">
+            <!-- DataTable for Users -->
+            <div class="table-responsive">
+                <table id="stakeholderTable" class="table table-bordered table-striped table-hover">
                     <thead>
                         <tr>
                             <th>id</th>
@@ -37,7 +37,71 @@
                             <th>Action</th>
                         </tr>
                     </thead>
-                    
+                    <tbody>
+                        <?php
+                        // Assuming you have already established a database connection
+                        $result = mysqli_query($connection, $sql);
+                        // Define your SQL query
+                        $sql = "
+                            SELECT
+                                user.username,
+                                CONCAT(user.firstname, ' ', user.lastname) AS homeowner,
+                                transaction.tx_id,
+                                transaction.tx_no,
+                                transaction_particulars.particular,
+                                transaction_particulars.period,
+                                transaction_particulars.fromDate,
+                                transaction_particulars.toDate,
+                                transaction_particulars.amount,
+                                transaction_particulars.verification
+                            FROM
+                                hoa_db.user
+                            INNER JOIN
+                                hoa_db.transaction
+                            ON
+                                user.user_id = transaction.user_id
+                            INNER JOIN
+                                hoa_db.transaction_particulars
+                            ON
+                                transaction.tx_id = transaction_particulars.tx_id;
+                        ";
+
+                        // Execute the query
+                        $result = mysqli_query($connection, $sql);
+
+                        // Check if the query was successful
+                        if ($result) {
+                            // Fetch data from the result set
+                            while ($row = mysqli_fetch_assoc($result)) {
+                                // Output or process each row of data
+                                echo "<tr>";
+                                echo "<td>" . $row['tx_id'] . "</td>";
+                                echo "<td>" . $row['username'] . " (" . $row['homeowner'] . ")</td>";
+                                echo "<td>";
+                                echo '<button type="button" class="btn btn-primary">Edit</button>';
+                                // Add more actions/buttons as needed
+                                echo "</td>";
+                                echo "</tr>";
+                            }
+
+                            // Free result set
+                            mysqli_free_result($result);
+                        } else {
+                            // If the query fails, display an error message
+                            echo '<tr><td colspan="3">Error: ' . mysqli_error($connection) . '</td></tr>';
+                        }
+
+                        // Close the database connection
+                        mysqli_close($connection);
+                        ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</section>
+
+
                     <tbody>
                         <!-- User data will be populated here -->
                     </tbody>
